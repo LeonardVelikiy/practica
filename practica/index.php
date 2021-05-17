affe1bf622949d6ed064421d947ab3ac82a218c8
LeonardVelikiy <83536042+LeonardVelikiy@users.noreply.github.com>
2021-05-17 22:39:29 +0400

    git init

---
+++ practica/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Благоустройство города</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="head">
+			<div class="logo">ЛОГО</div>
+			<span class="link_f">О сервисе</span>
+			<span class="link_s">Все сообщения</span>
+			<div class="auth">Войти</div>
+		</div>
+		<div class="checker">
+			<div>
+				<div>xxx</div>
+				<div>Заинтересованых<br>граждан</div>
+			</div>
+			<div>
+				<div>yyy</div>
+				<div>Решенных<br>проблем</div>
+			</div>
+		</div>
+		<div class="tagline">
+			<div>Слоган</div>
+			<div>Сообщить о проблеме</div>
+		</div>
+		<div class="solved_text">Последние решенные проблемы</div>
+		<div class="solved_p_item">
+			<div class="solved_item">
+				<div>Фото</div>
+				<div>Название</div>
+				<div>Описание</div>
+				<div>Категория</div>
+				<div>Временная метка</div>
+			</div>
+			<div class="solved_item">
+				<div>Фото</div>
+				<div>Название</div>
+				<div>Описание</div>
+				<div>Категория</div>
+				<div>Временная метка</div>
+			</div>
+			<div class="solved_item">
+				<div>Фото</div>
+				<div>Название</div>
+				<div>Описание</div>
+				<div>Категория</div>
+				<div>Временная метка</div>
+			</div>
+			<div class="solved_item">
+				<div>Фото</div>
+				<div>Название</div>
+				<div>Описание</div>
+				<div>Категория</div>
+				<div>Временная метка</div>
+			</div>
+			<div class="solved_item">
+				<div>Фото</div>
+				<div>Название</div>
+				<div>Описание</div>
+				<div>Категория</div>
+				<div>Временная метка</div>
+			</div>
+			<div class="solved_item">
+				<div>Фото</div>
+				<div>Название</div>
+				<div>Описание</div>
+				<div>Категория</div>
+				<div>Временная метка</div>
+			</div>
+			<div class="solved_item">
+				<div>Фото</div>
+				<div>Название</div>
+				<div>Описание</div>
+				<div>Категория</div>
+				<div>Временная метка</div>
+			</div>
+			<div class="solved_item">
+				<div>Фото</div>
+				<div>Название</div>
+				<div>Описание</div>
+				<div>Категория</div>
+				<div>Временная метка</div>
+			</div>
+		</div>
+		<div class="copyright">Copyright</div>
+	</div>
+</body>
+</html>
