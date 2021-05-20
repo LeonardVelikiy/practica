14f54539bdd881a460007c29b58fb332baec7ab5
Ivan <koshheev.ivan@bk.ru>
2021-05-20 17:35:22 +0400

    ппп

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Хочу член Саши</title>
+	<title>Благо</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="styles/main.css">
 </head>
