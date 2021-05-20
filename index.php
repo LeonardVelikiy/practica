b397e394e4b9d6fcc3fd6ca421b3f95e6e0e3663
Your Name <nik.golovash@bk.ru>
2021-05-20 18:17:21 +0400

    fix name

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Никита сосёт, тварь поганая</title>
+	<title>Благоустройство города</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="styles/main.css">
 </head>
