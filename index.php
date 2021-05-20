407ef1f3d811f4192d19adfbd5ba56c7af8dc7dd
Your Name <nik.golovash@bk.ru>
2021-05-20 16:23:29 +0400

    член

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>у сани маленький член</title>
+	<title>член</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="styles/main.css">
 </head>
