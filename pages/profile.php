b498e38565139922cb1c17bfa03cfd842e25c1c8
Your Name <nik.golovash@bk.ru>
2021-05-27 14:39:31 +0400

    + profile

---
+++ pages/profile.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Личный кабинет</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles/profile.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="head">
+			<div class="logo">ЛОГО</div>
+			<span class="link_f">О сервисе</span>
+			<span class="link_s">Все сообщения</span>
+			<div class="out">Выйти</div>
+		</div>
+		<span class="yinfo_text">Ваш профиль</span>
+		<div class="user_info">
+			<div class="avatar_img">Аватар</div>
+			<div class="std_info">
+				<div>Логин</div>
+				<div>Имя</div>
+				<div>Фамилия</div>
+				<div>Почта</div>
+			</div>
+			<div class="user_info_change">Изменить профиль</div>
+		</div>
+		<div class="problem_text">Ваши заявки</div>
+		<div class="your_p_item">
+			<div class="prodlem_item">
+				<div>Фото</div>
+				<div>Название</div>
+				<div>Описание</div>
+				<div>Категория</div>
+				<div>Временная метка</div>
+				<div><a href="">Удалить</a></div>
+			</div>
+		</div>
+		<div class="delete_acc">
+			<a href="">Удалить профиль</a>
+		</div>
+		<div class="copyright">Copyright</div>
+	</div>
+</body>
+</html>
