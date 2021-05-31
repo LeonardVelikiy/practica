b0ca5a52f27b40d5528de2290320d92cdc6d0f8c
Ivan <koshheev.ivan@bk.ru>
2021-05-31 16:55:09 +0400

    Панель администрации

---
+++ pages/administration.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Администрация</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../styles/administration.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="head">
+			<div class="logo">ЛОГО</div>
+			<span class="link_f">О сервисе</span>
+			<span class="link_s">Все сообщения</span>
+			<div class="out">Выйти</div>
+		</div>
+		<div>
+			<div class="left_panel">
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			<div class="left_panel_item"></div>
+			</div>
+			<div class="right_body">
+				<div class="problem_text">Панель администрации</div>
+			</div>
+		</div>
+		<div class="copyright">Copyright</div>
+	</div>
+</body>
+</html>
