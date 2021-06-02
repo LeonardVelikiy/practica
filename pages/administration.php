e9ee51805d2c56d061ba0d0bb49e30c623df0fa3
Your Name <nik.golovash@bk.ru>
2021-06-02 17:05:35 +0400

    fix

--- pages/administration.php
+++ pages/administration.php
@@ -28,11 +28,59 @@
 				include 'db.php';
 				if($_GET['applications'])
 				{
+					$str_applications_out=mysqli_query($connect, "SELECT * FROM `applications`");
+					echo"<table border=1 cellspacing=0 >
+					<tr>
+						<th style=text-aligin:center;>Публикующий
+						<th style=text-aligin:center;>статус
+						<th style=text-aligin:center;>название
+						<th style=text-aligin:center;>категория
+						<th style=text-aligin:center;>время публикации
+					</tr>
+						";
+						while($out=mysqli_fetch_array($str_applications_out))
+						{
+							echo"
+						<tr>	
+							<td>$out[user]
+							<td>$out[status]
+							<td>$out[title]
+							<td>$out[category]
+							<td>$out[date_start]
+						</tr>";
+						}
+					echo "</table>";
+					
+				}
+				if($_GET['users'])
+				{
+					$str_users_out=mysqli_query($connect, "SELECT * FROM `users`");
+					echo"<table border=1 cellspacing=0 >
+					<tr>
+						<th style=text-aligin:center;>ФИО
+						<th style=text-aligin:center;>Login
+						<th style=text-aligin:center;>Mail
+						<th style=text-aligin:center;>Заявки
+					</tr>
+						";
+						while($out=mysqli_fetch_array($str_users_out))
+						{
+							echo"
+						<tr>	
+							<td>$out[first_last_name]
+							<td>$out[login]
+							<td>$out[mail]
+							<td>0
+						</tr>";
+						}
+					echo "</table>";
+				}
+				if($_GET['aplications_work'])
+				{
 					$applications_del=$_GET['applications'];
 					$str_applications_del=mysqli_query($connect, "DELETE FROM `applications` WHERE id = $applications_del");
 
 					$str_applications_out=mysqli_query($connect, "SELECT * FROM `applications`");
-
 					echo"<table border=1 cellspacing=0 >
 					<tr>
 						<th style=text-aligin:center;>Публикующий
@@ -57,15 +105,14 @@
 						</tr>";
 						}
 					echo "</table>";
-					
 				}
-				if($_GET['users'])
+				if($_GET['users_work'])
 				{
 					$users_del=$_GET['users'];
 					$str_user_del=mysqli_query($connect, "DELETE FROM `users` WHERE id = $users_del");
 
 					$str_users_out=mysqli_query($connect, "SELECT * FROM `users`");
-					echo"<table border=1 cellspacing=0 >
+					echo"<table border=1 cellspacing=0 style=widht:50%;>
 					<tr>
 						<th style=text-aligin:center;>ФИО
 						<th style=text-aligin:center;>Login
@@ -83,18 +130,9 @@
 							<td>$out[mail]
 							<td>0
 							<td><a href=?users=$out[id]>удалить</a>
-							<td><a href=?users=$out[id]>подробнее</a>
 						</tr>";
 						}
 					echo "</table>";
-				}
-				if($_GET['aplications_work'])
-				{
-					
-				}
-				if($_GET['users_work'])
-				{
-					
 				}
 				
 				?>
