416fbdd35fa1e834c41e56322203f109052a1d4c
Sasha22813 <sasha.krykov.96@mail.ru>
2021-06-04 05:53:24 +0400

    fix

--- pages/all_messages.php
+++ pages/all_messages.php
@@ -26,6 +26,7 @@
 			<select class="select">
 				<option>Категория</option>
 				<?php
+				$connect=mysqli_connect('localhost','cn31570_practica','practica','cn31570_practica'); 
 		$str_out_categoty="SELECT * FROM `category`";
 		$run_out_categoty=mysqli_query($connect,$str_out_categoty);
 		while ($out=mysqli_fetch_array($run_out_categoty)){
