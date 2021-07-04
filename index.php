2a09ed2b50fe34b7a622ba50031fd080c2464ac9
Your Name <nik.golovash@bk.ru>
2021-07-04 10:09:31 +0400

    test

--- index.php
+++ index.php
@@ -113,10 +113,11 @@ $online_count = R::count('online', "lastvisit > " . ( time() - (360) ));
 				$reg=$_POST['reg'];
 				if($reg)
 				{
-					if($first_last_name and $login  and $Emai  and $pass==$copy_pass and $cb) 
+					if($first_last_name and $login and $Emai and $pass==$copy_pass and $cb) 
 					{
-						$str_user_plus=mysqli_query($connect, "INSERT INTO `user` (`first_last_name`, `mail`, `pass`, `login`) VALUES ('$first_last_name','$Email','$pass','$login');");
-						echo '<script>location.replace("../pages/profile.php");</script>'; exit;
+						// $str_user_plus=mysqli_query($connect, "INSERT INTO `user` (`first_last_name`, `mail`, `pass`, `login`) VALUES ('$first_last_name','$Email','$pass','$login');");
+						// echo '<script>location.replace("../pages/profile.php");</script>'; exit;
+						echo"круто";
 					}else
 					{
 						echo'<br>заполните все поля<br>';
