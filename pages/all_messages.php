eb96d5bbf38277e590a2390729ad603c8f984291
Sasha22813 <sasha.krykov.96@mail.ru>
2021-07-05 13:41:13 +0400

    fix

--- pages/all_messages.php
+++ pages/all_messages.php
@@ -9,8 +9,110 @@ $connect=mysqli_connect('localhost','cn31570_practica','practica','cn31570_pract
 	<link rel="stylesheet" type="text/css" href="../styles/all_messages.css">
 </head>
 <body>
+<div id=reg_dark>
+<div class="form_window_reg">
+		<a href="#">
+			<div class="close_btn"></div>
+		</a>
+		<div class="form_mname">Регистрация</div>
+		<div class="form_place">
+			<form method="POST">
+				<input type="text" name="first_last_name" placeholder="ФИО" class="form_mitem"><br>
+				<input type="text" name="login" placeholder="Логин" class="form_mitem"><br>
+				<input type="text" name="Email" placeholder="Email" class="form_mitem"><br>
+				<input type="password" name="pass" placeholder="Пароль" class="form_mitem"><br>
+				<input type="password" name="copy_pass" placeholder="Повторите пароль" class="form_mitem"><br>
+				<input type="checkbox" name="cb"><span class="pers_inf">Согласие на обработку<br>персональных данных</span><br>
+				<input type="submit" name="reg" value="Регистрация" class="form_btn_reg">
+				<?php
+				$first_last_name=$_POST['first_last_name'];
+				$login=$_POST['login'];
+				$Email=$_POST['Email'];
+				$pass=$_POST['pass'];
+				$copy_pass=$_POST['copy_pass'];
+				$cb=$_POST['cb'];
+				$reg=$_POST['reg'];
+				if($reg)
+				{	
+					if($copy_pass == $pass)
+					{
+							if($first_last_name and $login and $Email and $cb) 
+							{
+							$str_user_plus=mysqli_query($connect, "INSERT INTO `users` (`first_last_name`, `mail`, `pass`, `login`) VALUES ('$first_last_name','$Email','$pass','$login');");
+							header("Location: #auth_dark");exit();
+								
+							}else
+							{
+								echo'<br>заполните все поля<br>';
+							}
+					}
+					
+				}
+				?>
+			</form>
+		</div>
+		<div class="form_link"><a href="#auth_dark">Войти</a></div>
+	</div>
+</div>
+<div id=auth_dark>
+<div class="form_window_auth">
+		<a href="#">
+			<div class="close_btn"></div>
+		</a>
+		<div class="form_mname">Вход</div>
+		<div class="form_place">
+			<form method="POST">
+				<input type="text" name="login" placeholder="Логин" class="form_mitem"><br>
+				<input type="text" name="pass" placeholder="Пароль" class="form_mitem"><br>
+				<input type="submit" name="auth" value="Вход" class="form_btn">
+			
+			<?php
+			$login=$_POST['login'];
+			$pass=$_POST['pass'];
+			$add=$_POST['auth'];
+			if ($add) {
+				$_SESSION['login']=$login;
+				$_SESSION['pass']=$pass;
+				$_SESSION['auth']=$add;
+			}
+			if($add)
+			{
+				$str_auth="SELECT * FROM `users` WHERE `login` = '$_SESSION[login]' AND `pass` = '$_SESSION[pass]'";
+				$run_auth=mysqli_query($connect,$str_auth);
+				$check_users=mysqli_num_rows($run_auth);
+
+				if ($check_users) 
+					{
+						$user= mysqli_fetch_assoc($run_auth);
+						if ($user['role']==0) 
+						{
+							 echo '<script>location.replace("../pages/profile.php");</script>'; exit;
+						}else
+						{
+							 echo '<script>location.replace("../pages/administration.php");</script>'; exit;
+						}
+									
+					}else
+					{
+						echo '<script>location.replace("/");</script>'; exit;
+					
+						unset($_SESSION);
+					}
+
+			}
+
+
+			?>
+			</form>
+		</div>
+		<div class="form_link"><a href="#reg_dark">Регистрация</a></div>
+	</div>
+</div>
 <div id=dark_success>
 <div id=okno_success>
+<a href="#">
+			<div class="close_btn"></div>
+		</a>
 <form method="POST" enctype="multipart/form-data">
 <input type="file" name="photo_end" class="form_mitem1" id="form_mitem1"><br><br>
 <input type="submit" name="add" class="form_mbtn1" value="Подтвердить">
@@ -73,9 +175,23 @@ echo "Заполните поля";
 			<a href="all_messages.php">
 				<div class="link_s">Все сообщения</div>
 			</a>
-			<a href="auth_form.php">
-				<div class="auth">Войти</div>
-			</a>
+			<?php
+			if ($_SESSION['login'] == NULL) {
+			echo "<a href=#auth_dark>
+				<div class=auth>Войти</div>
+			</a>";
+			}
+			else
+			{
+				echo "<a href=../pages/profile.php><div class=kab>Мой кабинет</div></a><form method=POST><input type=submit name=exit value=Выход class=exit></form>";
+			}
+			$exit=$_POST['exit'];
+			if ($exit) {
+				session_destroy();
+				echo '<script>location.replace("index.php");</script>';
+				exit();
+			}
+			?>
 		</div>
 		<div class="mess_text">Все сообщения</div>
 		<div class="all_messages_filter">
