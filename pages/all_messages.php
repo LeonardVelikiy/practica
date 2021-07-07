f0b2b2c6aeda8c782bb476d8f53799d28ca500f4
Sasha22813 <sasha.krykov.96@mail.ru>
2021-07-07 13:48:03 +0400

    fix

--- pages/all_messages.php
+++ pages/all_messages.php
@@ -204,7 +204,7 @@ echo "Заполните поля";
 		<div class="all_messages_filter">
 			<form>
 			<form method=GET><br>
-			<input type=text name=search class=form_mitem1><br>
+			<input type=text name=search class=form_mitem1>
 				<select class="select" name=select>
 				<option>Категория</option>
 				<?php
