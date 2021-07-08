c5a65ebee1b1402d3a31e5d3611302e9d061dd3e
Sasha22813 <sasha.krykov.96@mail.ru>
2021-07-08 07:32:08 +0400

    fix

--- pages/profile.php
+++ pages/profile.php
@@ -38,6 +38,7 @@ $login_edit=$_POST['login_edit'];
 $name_edit=$_POST['name_edit'];
 $mail_edit=$_POST['mail_edit'];
 $avatar=$_POST['avatar'];
+$save=$_POST['save'];
 $str_upd_user="UPDATE `users` SET `avatar`='$avatar',`first_last_name`='$name_edit',`mail`='$mail_edit',`login`='$login_edit'";
 if ($save){
 $run_upd_user=mysqli_query($connect, $str_upd_user);
