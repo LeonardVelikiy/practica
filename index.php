b1662d3610f945f49b350ed7c109ed78031863e6
Sasha22813 <sasha.krykov.96@mail.ru>
2021-07-27 03:05:11 +0400

    fix

--- index.php
+++ index.php
@@ -289,7 +289,12 @@ else
 			{
 				$str_auth="SELECT * FROM `users` WHERE `login` = '$_SESSION[login]' AND `pass` = '$_SESSION[pass]'";
 				$run_auth=mysqli_query($connect,$str_auth);
-				$user= mysqli_fetch_assoc($run_auth);
+
+				$check_users=mysqli_num_rows($run_auth);
+
+				if ($check_users) 
+					{
+						$user= mysqli_fetch_assoc($run_auth);
 			if ($user['role']=0){
 				echo "<a href=../pages/profile.php><div class=kab>Мой кабинет</div></a><form method=POST><input type=submit name=exit value=Выход class=exit></form>";
 				}
@@ -297,6 +302,7 @@ else
 				{
 					echo "<a href=../pages/administration.php><div class=kab>Мой кабинет</div></a><form method=POST><input type=submit name=exit value=Выход class=exit></form>";
 				}
+			}
 			}
 			$exit=$_POST['exit'];
 			if ($exit) {
