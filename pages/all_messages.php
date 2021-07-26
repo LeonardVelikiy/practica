bb4508a55da75b17c0e14c3bd2d3acd3e188f29b
Sasha22813 <sasha.krykov.96@mail.ru>
2021-07-27 02:03:27 +0400

    fix

--- pages/all_messages.php
+++ pages/all_messages.php
@@ -58,7 +58,6 @@ $connect=mysqli_connect('localhost','cn31570_practica','practica','cn31570_pract
 							else
 							{
 								echo "Ошибка регистрации";
-								print_r($str_user_plus);
 							}
 							}else
 							{
